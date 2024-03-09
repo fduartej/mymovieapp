05d5cc9548729ed769e967665d7fee4602445b7b
Fred Duarte <fduartej@gmail.com>
2024-03-08 21:26:12 -0500

    Form cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mymovieapp.Models;
+
+namespace mymovieapp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Nombre,Apellido,CorreoElectronico,Telefono,FechaNacimiento")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Message"] = "El cliente se ha registrado";
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,16 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mymovieapp.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre del cliente")]
+        [Required]
         public string? Nombre { get; set; }
+
+        [Display(Name = "Apellido del cliente")]
+        [Required]
         public string? Apellido { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Correo Electronico")]
+        [Required]
         public string? CorreoElectronico { get; set; }
+
+        [Phone]
+        [Required]
         public string? Telefono { get; set; }
         public DateTime FechaNacimiento { get; set; }
     }
